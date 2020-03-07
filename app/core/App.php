9c571fc72bd8230e9e8f0bb101f1049dd942090c
deswandamanik <deswandamanik@gmail.com>
2020-03-07 23:16:26 +0700

    merubah aturan url pada file app.php

--- app/core/App.php
+++ app/core/App.php
@@ -10,7 +10,12 @@ class App {
   public function parseURL()
   {
     if(isset($_GET['url'])){
-      $url = $_GET['url'];
+      //menghilangkan tanda "/"di url
+      $url = rtrim($_GET['url'],'/');
+      //memberikan filter agar bersih dari karakter aneh, url tidak mudah di serang
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      // memecah url berdasarkan tanda "/",string berubah jadi element array
+      $url = explode('/', $url);
       return $url;
     }
   }
