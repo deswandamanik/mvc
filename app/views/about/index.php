20df99e776e5681ec946cf0a8278a3994b5ae0f6
deswandamanik <deswandamanik@gmail.com>
2020-03-17 15:39:52 +0700

    berhasil mengelola ASSETS pada aplikasi MVC , aset seperti CSS, Javascript dan juga gambar

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-
-  <h1>About Me</h1>
+<div class="container">
+  <h1 class="mt-5">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Deswanto Damanik" width="200" class="rounded-circle shadow">
   <p>Hallo, Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya seorang <?= $data['pekerjaan']; ?> .</p>
+
+  </div>
