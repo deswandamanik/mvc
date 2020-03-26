0b03bc10991f469ec811ecab8d09f531aba0e8f1
deswandamanik <deswandamanik@gmail.com>
2020-03-27 01:34:03 +0700

    berhasil menambahkan fitur UPDATE DATA pada aplikasi MVC. Update data ini masih menggunakan bantuan dari modal box form tambah, yang nilai2 di dalam formnya diambil dan ditampilkan menggunakan AJAX.

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -8,7 +8,7 @@
 
   <div class="row">
     <div class="col-6-lg-6">
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+      <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
         Tambah Data Mahasiswa
       </button>
       <br>
@@ -19,6 +19,7 @@
           <li class="list-group-item ">
             <?= $mhs ['nama']; ?>
               <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs ['id']; ?>" class="badge badge-danger float-right ml-1" onclick="return confirm('yakin?');">hapus</a>
+              <a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs ['id']; ?>" class="badge badge-success float-right tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?= $mhs['id']; ?>" >ubah</a>
               <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs ['id']; ?>" class="badge badge-primary float-right ml-1">detail</a>
           </li>
       <?php endforeach; ?>
@@ -35,13 +36,14 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
         <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+          <input type="hidden" name="id" id="id">
           <div class="form-group">
             <label for="nama">Nama</label>
             <input type="text" class="form-control" id="nama" name="nama">
@@ -67,11 +69,13 @@
 
             </select>
           </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Tambah Data</button>
-      </form>
+      
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Tambah Data</button>
+            </div>
+        </form>
+          
       </div>
     </div>
   </div>
